5637aeeea46ffced8421aaacaa79feee7ef2239f
DigitalDetective47 <ninji2701@gmail.com>
2025-01-27 20:14:54 -0500

    Ceremonial Dagger

---
+++ localization/en-us.lua
@@ -0,0 +1,15 @@
+return {
+    descriptions = {
+        Joker = {
+            j_ceremonial = {
+                text = {
+                    "When {C:attention}Blind{} is selected,",
+                    "destroy Joker to the right",
+                    "and permanently add {C:attention}#2#",
+                    "its sell value to this {C:red}Mult",
+                    "{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)",
+                },
+            },
+        }
+    }
+}
--- main.lua
+++ main.lua
@@ -32,3 +32,45 @@ SMODS.Joker:take_ownership('j_wily', poker_hand, true)
 SMODS.Joker:take_ownership('j_clever', poker_hand, true)
 SMODS.Joker:take_ownership('j_devious', poker_hand, true)
 SMODS.Joker:take_ownership('j_crafty', poker_hand, true)
+
+SMODS.Joker:take_ownership('j_ceremonial', {
+    config = { mult = 0, ratio = 2 },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.mult, card.ability.ratio == 2 and "double" or tostring(card.ability.ratio) .. "X" } }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then
+            return { mult = card.ability.mult }
+        elseif context.setting_blind and not self.getting_sliced and not context.blueprint then
+            local my_pos = nil
+            for i = 1, #G.jokers.cards do
+                if G.jokers.cards[i] == card then
+                    my_pos = i
+                    break
+                end
+            end
+            if my_pos and G.jokers.cards[my_pos + 1] and not self.getting_sliced and not G.jokers.cards[my_pos + 1].ability.eternal and not G.jokers.cards[my_pos + 1].getting_sliced then
+                local sliced_card = G.jokers.cards[my_pos + 1]
+                sliced_card.getting_sliced = true
+                G.GAME.joker_buffer = G.GAME.joker_buffer - 1
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        G.GAME.joker_buffer = 0
+                        card.ability.mult = card.ability.mult + sliced_card.sell_cost * card.ability.ratio
+                        card:juice_up(0.8, 0.8)
+                        sliced_card:start_dissolve({ HEX("57ecab") }, nil, 1.6)
+                        play_sound('slice1', 0.96 + math.random() * 0.08)
+                        return true
+                    end
+                }))
+                card_eval_status_text(card, 'extra', nil, nil, nil,
+                    {
+                        message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.mult + 2 * sliced_card.sell_cost } },
+                        colour =
+                            G.C.RED,
+                        no_juice = true
+                    })
+            end
+        end
+    end,
+}, true)
